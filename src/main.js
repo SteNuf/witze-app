dfc1ac0f2754cba514b3798a1d0e95d101d09e9d
SteNuf <stephan.nufer@gmx.de>
2025-09-01 14:22:42 +0200

    This  commit add the button to save and install the API with the html.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 import "./main.scss";
+import "./fetch.js";
+
+const newJokeButtonElement = document.getElementById("joke-new-button");
+
+newJokeButtonElement.addEventListener("click", getJoke);
+
+function getJoke() {
+  getJokeAPI();
+}
+
+async function getJokeAPI() {
+  const response = await fetch("https://witzapi.de/api/joke/");
+
+  const body = await response.json();
+  console.log(body);
+
+  const jokeArray = body;
+
+  console.log(jokeArray[0].text);
+
+  const jokeOutput = document.getElementById("text");
+  jokeOutput.innerHTML = jokeArray[0].text;
+}
