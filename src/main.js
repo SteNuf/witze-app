47506c930fb1122f1b5ee46e2d2ef1e851540964
SteNuf <stephan.nufer@gmx.de>
2025-09-05 22:39:18 +0200

    This commit add the display button, if Klick the New Joke Button.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import "./main.scss";
 
 const newJokeButtonElement = document.querySelector(".joke__new-button");
 const saveJokeButtonElement = document.querySelector(".joke__save-button");
-const jokeSaveListElement = document.querySelector(".joke-save__list");
+//const jokeSaveListElement = document.querySelector(".joke-save__list");
+
+const hidden = document.getElementById("hidden-button");
+const hiddenSaveJoke = document.getElementById("hidden-joke");
 
 newJokeButtonElement.addEventListener("click", getJoke);
+newJokeButtonElement.addEventListener("click", () => {
+  hidden.style.display = "inline-block";
+});
+
+newJokeButtonElement.addEventListener("click", () => {
+  hiddenSaveJoke.style.display = "none";
+});
+
 saveJokeButtonElement.addEventListener("click", saveLocalStorageJoke);
 
 let joke = getLocalStorageJoke();
